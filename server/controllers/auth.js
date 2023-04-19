401a42decf28052911ae5aa1ad3b5aca7f084949
xaviervide <xaviervidef@gmail.com>
2023-04-19 18:07:56 +0100

    feat!: user correctly logging in generates a JWT token to handle authorization

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,3 +1,4 @@
+import 'dotenv/config.js';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import User from '../models/userModel.js';
@@ -5,6 +6,7 @@ import User from '../models/userModel.js';
 /* REGISTER USER */
 export const registerUser = async (req, res) => {
   try {
+    //RETRIEVE DATA FROM THE REQUEST BODY
     const {
       firstName,
       lastName,
@@ -13,9 +15,11 @@ export const registerUser = async (req, res) => {
       password
     } = req.body;
 
+    //GENERAT SALT AND HASH PASSWORD
     const salt = await bcrypt.genSalt();
     const passwordHash = await bcrypt.hash(password, salt);
 
+    //CREATE A NEW USER WITH THE HASHED PASSWORD
     const newUser = new User({
       firstName,
       lastName,
@@ -24,11 +28,59 @@ export const registerUser = async (req, res) => {
       password: passwordHash
     })
 
+    //SAVE NEW USER TO DATABASE
     const registeredUser = await newUser.save();
+
+    //HANDLE RESPONSE
     res.status(201);
     res.send(registeredUser);
   } catch (err) {
+    //HANDLE ERROR
     console.log('ERROR REGISTERING USER: ', err);
+    res.status(500);
+    res.end();
+  }
+}
+
+/* USER LOG IN */
+export const loginUser = async (req, res) => {
+  try {
+    //RETRIEVE DATA FROM REQUEST BODY
+    const {
+      email,
+      password
+    } = req.body;
+
+    //CHECK IF IT'S A VALID EMAIL (USER WITH THAT EMAIL ALREADY EXISTS)
+    const user = await User.findOne({ email: email });
+
+    //IF IT DOES NOT EXIST (INVALID EMAIL)
+    if (!user) {
+      res.status(400);
+      console.log('ERROR: User does not exist!');
+      res.end();
+    }
+
+    //IF IT IS A VALID EMAIL, CHECK IF PASSWORD IS CORRECT
+    const passwordIsValid = await bcrypt.compare(password, user.password);
+
+    //IF PASSWORD IS INCORRECT
+    if (!passwordIsValid) {
+      res.status(400);
+      console.log('ERROR: Invalid credentials!');
+      res.end(); 
+    }
+
+    //IF PASSWORD IS CORRECT, AUTHENTICATE USER USING JWT
+    const token = jwt.sign({ id: user._id}, process.env.JWT_SECRET_KEY);
+    //DELETE UNHASHED PASSWORD BEFORE SENDING IT BACK
+    delete user.password;
+    //HANDLE RESPONSE
+    res.status(200);
+    res.send({ token, user });
+
+  } catch (err) {
+    console.log('ERROR LOGGING IN USER');
     res.status(500);
   }
 }
