52c8726acce5d58f4b53dd41ea8f2f61b04c815a
xaviervide <xaviervidef@gmail.com>
2023-04-19 17:16:06 +0100

    feat!: register user with encrypted password using bcrypt

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,5 +1,34 @@
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
-import userModel from '../models/userModel.js';
+import User from '../models/userModel.js';
 
 /* REGISTER USER */
+export const registerUser = async (req, res) => {
+  try {
+    const {
+      firstName,
+      lastName,
+      profilePicPath,
+      email,
+      password
+    } = req.body;
+
+    const salt = await bcrypt.genSalt();
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      firstName,
+      lastName,
+      profilePicPath,
+      email,
+      password: passwordHash
+    })
+
+    const registeredUser = await newUser.save();
+    res.status(201);
+    res.send(registeredUser);
+  } catch (err) {
+    console.log('ERROR REGISTERING USER: ', err);
+    res.status(500);
+  }
+}
--- server/router.js
+++ server/router.js
@@ -1,5 +1,8 @@
 import express from 'express';
+import { registerUser } from './controllers/auth.js';
 
-const router = express.Router();  
+const router = express.Router();
+
+router.post('/register', registerUser);
 
 export default router;
