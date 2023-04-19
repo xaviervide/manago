cf1fcbf8bd7deee98c4600806e7072b19c8d9e89
xaviervide <xaviervidef@gmail.com>
2023-04-19 15:40:42 +0100

    feat: added router.js

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
+import router from './router.js';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(router);
 
 app.listen(process.env.PORT, () => {
   console.log(`🚀 Server up and running on port ${process.env.PORT}`);
