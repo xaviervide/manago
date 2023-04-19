1e04ff247f1374cfa4e4174883b05212dfb78c30
xaviervide <xaviervidef@gmail.com>
2023-04-19 16:33:42 +0100

    fix: cleaned userModel.js to just hold schema

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -10,8 +10,4 @@ const userSchema = mongoose.Schema({
 
 const User = mongoose.model('User', userSchema);
 
-export function postOne (user) {
-  const newUser = new User(user);
-  newUser.save()
-    .catch(err => console.log('ERROR SAVING USER: ', err));
-}
+export default User;
