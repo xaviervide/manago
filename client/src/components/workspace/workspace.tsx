e408a94034e9dbe48e5062ab97ec6fa7702bd2ce
xaviervide <xaviervidef@gmail.com>
2023-04-21 11:18:51 +0100

    feat: added workspaceShelf component and its wireframe

--- client/src/components/workspace/workspace.tsx
+++ client/src/components/workspace/workspace.tsx
@@ -1,8 +1,10 @@
+import WorkspaceShelf from '../workspaceShelf/workspaceShelf';
 import './workspace.css';
 
 function Workspace () {
   return (
     <div className="workspace-container">
+      <WorkspaceShelf></WorkspaceShelf>
     </div>
   );
 }
---
+++ client/src/components/workspaceShelf/workspaceShelf.tsx
@@ -0,0 +1,30 @@
+import './workspaceShelf.css';
+
+function WorkspaceShelf () {
+  return (
+    <div className="workspaceshelf-container">
+      <div className="shelf-title-container">
+        <h2>YOUR WORKSPACE</h2>
+      </div>
+      <div className="workspace-projects-container">
+        <h4>One Project</h4>
+        <h4>One Project</h4>
+        <h4>One Project</h4>
+        <h4>One Project</h4>
+        <h4>One Project</h4>
+        <h4>One Project</h4>
+        <h4>One Project</h4>
+        <h4>One Project</h4>
+        <h4>One Project</h4>
+        <h4>One Project</h4>
+        <h4>One Project</h4>
+        <h4>One Project</h4>
+        <h4>One Project</h4>
+        <h4>One Project</h4>
+        <h4>One Project</h4>
+      </div>
+    </div>
+  );
+}
+
+export default WorkspaceShelf;
