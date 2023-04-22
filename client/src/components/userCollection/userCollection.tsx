0df664a932ec9c485a1eca25d540a7410c7d5688
xaviervide <xaviervidef@gmail.com>
2023-04-22 19:19:49 +0100

    feat: collections now collapse on click

--- client/src/components/userCollection/userCollection.tsx
+++ client/src/components/userCollection/userCollection.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import WsCard from '../wsCard/wsCard';
 import './userCollection.css';
 
@@ -8,16 +9,30 @@ interface UserCollectionProps {
 }
 
 function UserCollection ({isProjects} : UserCollectionProps) {
+
+  const [isCollapsed, setIsCollapsed] = useState(true);
+
+  function collapseCollection () {
+    setIsCollapsed(!isCollapsed);
+  }
+
   return (
     <div className="usercollection-container">
       <div className="usercollection-label-container">
-        <h3 className="usercollection-label">Your {isProjects ? "Projects" : "Teams"}</h3>
+        <h3 className="usercollection-label" onClick={() => collapseCollection()}>Your {isProjects ? "Projects" : "Teams"}</h3>
       </div>
+      {!isCollapsed && 
       <div className="usercollection-projects-container">
-      <WsCard></WsCard>
-      <WsCard></WsCard>
-      <WsCard></WsCard>
+        <WsCard></WsCard>
+        <WsCard></WsCard>
+        <WsCard></WsCard>
+        <WsCard></WsCard>
+        <WsCard></WsCard>
+        <WsCard></WsCard>
+        <WsCard></WsCard>
+        <WsCard></WsCard>
       </div>
+      }
     </div>
   );
 }
